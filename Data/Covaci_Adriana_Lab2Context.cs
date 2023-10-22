f1c7bc4eabe55cdadebfa2d45f1badd518dbfa33
Adriana Covaci <adrianacovaci22@gmail.com>
2023-10-22 23:07:15 +0300

    Add project files.

---
+++ Data/Covaci_Adriana_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Covaci_Adriana_Lab2.Models;
+
+namespace Covaci_Adriana_Lab2.Data
+{
+    public class Covaci_Adriana_Lab2Context : DbContext
+    {
+        public Covaci_Adriana_Lab2Context (DbContextOptions<Covaci_Adriana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Covaci_Adriana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Covaci_Adriana_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
