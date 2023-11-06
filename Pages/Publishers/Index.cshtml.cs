4f60e93436abd4d42926b367f85a8aae87e47cf5
Adriana Covaci <adrianacovaci22@gmail.com>
2023-11-06 17:41:34 +0200

    lab4

--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,12 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Covaci_Adriana_Lab2.Data;
 using Covaci_Adriana_Lab2.Models;
+using Covaci_Adriana_Lab2.Models.ViewModels;
 
 namespace Covaci_Adriana_Lab2.Pages.Publishers
 {
     public class IndexModel : PageModel
     {
         private readonly Covaci_Adriana_Lab2.Data.Covaci_Adriana_Lab2Context _context;
+       
 
         public IndexModel(Covaci_Adriana_Lab2.Data.Covaci_Adriana_Lab2Context context)
         {
@@ -21,13 +23,23 @@ namespace Covaci_Adriana_Lab2.Pages.Publishers
 
         public IList<Publisher> Publisher { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-           
-            if (_context.Publisher != null)
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+            .Include(i => i.Books)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.PublisherName)
+            .ToListAsync();
+            if (id != null)
             {
-                Publisher = await _context.Publisher.ToListAsync();
-               
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
             }
         }
     }
